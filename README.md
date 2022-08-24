33be4e0d8225f6af9f31fb43c39a805fd914faf6
Jachimo <jachimo@noreply.github.com>
2022-08-24 11:53:18 -0700

    Good enough for the moment; works okay.

--- README.md
+++ README.md
@@ -1,24 +1,30 @@
 # obstagtools
 
-Quick tools for working with Obsidian-flavor (YAML frontmatter + Markdown content) notes files.
+Some tools for working with Obsidian-flavor (YAML frontmatter + Markdown content) notes files.
 
 ## add_tag.py
 
-Quick-and-dirty tool for mass-adding tags to a Markdown document,
+Quick-and-dirty tool for batch-adding tags to a Markdown document,
 meant for scripting.
 
 Very minimal dependencies (only `sys`, `argparse`, and `logging`).
 
-**Usage** (in-place modification): `./add_tag.py targetfile.md -t tag1 tag2`  
-Or to create a copy on write (safer!): `./add_tag.py targetfile.md copyfile.md -t tag1 tag2`
+**Usage:**  
+- In-place modification: `./add_tag.py targetfile.md -t tag1 tag2`  
+- Copy-on-write (safer!): `./add_tag.py targetfile.md copyfile.md -t tag1 tag2`
 
-**Effect**: Prepends one or more specified tag values (`tag1`, `tag2`) to the list of tags in the YAML frontmatter.
+Tags can be either single-level (e.g. `cats`) or nested (`predators/cats`).
+If a desired tag contains spaces, enclose it in double-quotes;
+otherwise, tags are assumed to be space-delimited.
+
+**Effect**:  
+- _Prepends_ one or more specified tag values (`tag1`, `tag2`) to the list of tags in the YAML frontmatter.
 
 The specified input file must be a "well formed" YAML+Markdown document, consisting of: 
 
-- The string `---` on a line by itself (followed by a line ending character),
+- The string `---` on a line by itself (i.e. three hyphens, followed by a line ending character),
 - YAML frontmatter, specifically containing a sequence named 'tags' with one or more values, _using indented (not inline-block) notation_.
-- Another `---` on a line by itself (followed by a line ending char)
+- Another `---` on a line by itself
 - The content, formatted with Markdown
 
 Note the restriction on indented notation only for "tags". 
@@ -42,4 +48,14 @@ tags: [toys, gifts]
 # Heading
 Content begins here...
 ```
+### Scripting Examples
 
+Add a tag to all Markdown (`*.md`) files in a specified directory, recursively, that are newer than a specified file:
+
+```shell
+find ~/MyVault.obs/ \
+-type f \
+-name "*.md" \
+-newer ~/MyVault.obs/Travel/san-diego-departure.md \
+-exec /path/to/obstagtools/add_tag.py {} -t vacation places/sandiego \;
+```
--- add_tag.py
+++ add_tag.py
@@ -10,8 +10,8 @@ def main() -> int:  # returns Unix exit value
     parser = argparse.ArgumentParser(description='Add YAML frontmatter tags to Obsidian-style Markdown files')
     parser.add_argument('inpath', help='Input file to add tags to')
     parser.add_argument('outpath', nargs='?', default=False, help='Output file to write to (in-place modification if not given)')
-    parser.add_argument('--tag', '-t', nargs='+', help='Tag values to add to the document')  # TODO: should support multiple tags
-    parser.add_argument('--debug', help='Enable debug mode (very verbose output)', action='store_true')
+    parser.add_argument('--tag', '-t', nargs='+', help='Tag values to add to the document')
+    parser.add_argument('--debug', help='Enable debug mode (verbose output)', action='store_true')
     args = parser.parse_args()
 
     if args.debug:
@@ -20,30 +20,33 @@ def main() -> int:  # returns Unix exit value
     else:
         logging.basicConfig(level=logging.INFO)
 
-    logging.debug(f'Reading from {args.inpath}')
-
     if not args.tag:
         logging.info('No tag(s) specified, exiting')
         return 1
 
     if not args.outpath:
-        logging.info('WARNING: No output path specified, performing in-place modification')
+        logging.debug('No output path specified, performing in-place modification')
         args.outpath = args.inpath
 
+    obsdoc: obs_document.ObsDocument
     obsdoc = obs_document.ObsDocument()  # see obs_document.py
 
+    logging.debug(f'Reading from {args.inpath}')
     with open(args.inpath, 'r') as infile:
+        obsdoc.filename = args.inpath
         obsdoc.lines = infile.readlines()
         logging.debug(f'Read {len(obsdoc.lines)} lines')
 
+    t: str
     for t in args.tag:
         add_tag(obsdoc, t)
 
+    logging.debug(f'Writing to {args.outpath}')
     with open(args.outpath, 'w') as outfile:
-        logging.debug(f'Writing to {args.outpath}')
         outfile.writelines(obsdoc.lines)
-        logging.debug(f'Wrote {len(obsdoc.lines)}')
+        logging.debug(f'Wrote {len(obsdoc.lines)} lines')
 
+    logging.debug('Exiting successfully')
     return 0
 
 
@@ -51,7 +54,7 @@ def add_tag(doc: obs_document.ObsDocument, tag: str) -> obs_document.ObsDocument
     """Adds specified tag to the lines contained in the ObsDocument object"""
 
     if not doc.isWellFormed():
-        logging.debug('Input file does not appear to begin with well-formed YAML, exiting')
+        logging.info('File does not appear to begin with well-formed YAML frontmatter, exiting')
         raise ValueError
 
     newtagstr: str = '  - ' + tag.strip() + '\n'  # Note we are using two spaces, a dash -, a space, and the value
