ef2f21ab6400b9b872c0d8efe93f650a0ef95c31
Jachimo <Jachimo@users.noreply.github.com>
2022-08-31 03:06:54 -0400

    WIP: late night, seems to be shaping up

--- add_taxonomy.py
+++ add_taxonomy.py
@@ -15,8 +15,8 @@ def main() -> int:  # returns Unix exit value
     parser.add_argument('outpath', nargs='?', default=False,
                         help='Output file to write to (in-place modification if not given)')
     parser.add_argument('--taxonomy', '-T', default=False, help='YAML taxonomy file (default: metadata.yaml)')
-    parser.add_argument('--no-clean', help='Keep existing metadata fields not present in taxonomy',
-                        action='store_true')
+    parser.add_argument('--clean', help='Remove all input document fields not present in taxonomy',
+                        default=False, action='store_true')
     parser.add_argument('--debug', help='Enable debug mode (verbose output)', action='store_true')
     args = parser.parse_args()
 
@@ -46,16 +46,45 @@ def main() -> int:  # returns Unix exit value
     # Retrieve YAML frontmatter portion of the Obsidian doc
     metadata: dict = yaml.safe_load(obsdoc.get_frontmatter())
     logging.debug(f'Frontmatter YAML parsed as:\n{metadata}\n')
-    yaml.dump(metadata, sys.stdout)  # remove after debugging
+    #yaml.dump(metadata, sys.stdout)  # remove after debugging
 
-    with open(args.taxonomy, 'r') as taxofile:
+    with open(args.taxonomy, 'r') as tf:
         # Read JSON (or YAML? or simpler?) taxonomy file specifying metadata fields, types, and default values
-        pass
+        taxo: dict = yaml.safe_load(tf)
+        logging.debug(f'Taxonomy YAML parsed as:\n{taxo}\n')
 
     # Construct new frontmatter using taxonomy as definition
     #   - If field is in taxonomy AND in document already, keep and use document value
     #   - If field is in taxonomy BUT NOT in document already, add it and use default value
-    #   - IF NO-CLEAN SELECTED: If field is *not* in taxonomy BUT IS in document, keep and use doc value
+    #   - IF NOT --clean: If field is in document BUT NOT in taxonomy, keep and use document value
+    #   - IF --clean: If field is in document BUT NOT in taxonomy, remove it
+
+    newfm: dict = {}  # new frontmatter
+    keylist: list = []
+
+    if args.clean:  # if --clean is selected
+        keylist = list(taxo.keys())  # iterate over *only* keys in the taxonomy (effectively removes all others)
+    else:  # but normally, when --clean is *not* selected
+        keylist = list(set(taxo.keys()).union(set(metadata.keys())))  # iterate over all keys in *both* lists
+
+    for k in keylist:  # see above how keylist depends on --clean option
+        if k in metadata:
+            if (type(metadata[k]) is list) or (type(taxo[k]) is list):  # if the doc or taxo value is a list
+                if type(metadata[k]) is str:
+                    metadata[k] = [metadata[k]]  # convert str into 1-item list
+                if type(taxo[k]) is str:
+                    taxo[k] = [taxo[k]]
+                newfm[k] = taxo[k] + metadata[k]  # combine lists
+            else:
+                newfm[k] = metadata[k]  # for single-valued fields, prefer value on document
+        else:
+            newfm[k] = taxo[k]  # use default value from taxonomy file
+
+    logging.debug(f'Keeping {len(newfm)} fields, from {len(metadata.keys())} document '
+                  f'and {len(taxo.keys())} taxonomy fields '
+                  f'({len([key for key in taxo.keys() or metadata.keys()])} total unique)')
+    assert len(newfm) == len([key for key in taxo.keys() or metadata.keys()])
+    logging.debug('New metadata:\n' + str(newfm))
 
     # Regenerate YAML frontmatter and combine with content to make new document text
 
