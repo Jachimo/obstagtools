88c160d2d3798ba0ed70eae480bab8f5349c9e1c
Jachimo <jachimo@noreply.github.com>
2022-09-01 18:19:01 -0400

    The power of PEP 8 compels me.

--- add_tag.py
+++ add_tag.py
@@ -9,7 +9,8 @@ import obs_document
 def main() -> int:  # returns Unix exit value
     parser = argparse.ArgumentParser(description='Add YAML frontmatter tags to Obsidian-style Markdown files')
     parser.add_argument('inpath', help='Input file to add tags to')
-    parser.add_argument('outpath', nargs='?', default=False, help='Output file to write to (in-place modification if not given)')
+    parser.add_argument('outpath', nargs='?', default=False,
+                        help='Output file to write to (in-place modification if not given)')
     parser.add_argument('--tag', '-t', nargs='+', help='Tag values to add to the document')
     parser.add_argument('--debug', help='Enable debug mode (verbose output)', action='store_true')
     args = parser.parse_args()
