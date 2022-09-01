5ee68352867682cf5cb8a61aa56d611c21fd7fe9
Jachimo <jachimo@noreply.github.com>
2022-09-01 18:51:38 -0400

    WIP: Seems to work except for indentation

--- add_taxonomy.py
+++ add_taxonomy.py
@@ -2,7 +2,6 @@
 import argparse
 import logging
 import sys
-from typing import TextIO
 
 import oyaml as yaml
 
@@ -11,11 +10,13 @@ import obs_document
 
 def main() -> int:  # returns Unix exit value
     parser = argparse.ArgumentParser(description='Reformat Obsidian frontmatter to conform to a specified taxonomy')
-    parser.add_argument('inpath', help='Input file to add tags to')
+    parser.add_argument('inpath', help='Input file to read from')
     parser.add_argument('outpath', nargs='?', default=False,
-                        help='Output file to write to (in-place modification if not given)')
-    parser.add_argument('--taxonomy', '-T', default=False, help='YAML taxonomy file (default: metadata.yaml)')
-    parser.add_argument('--clean', help='Remove all input document fields not present in taxonomy',
+                        help='Output file to write to (if not provided, modify input file in-place)')
+    parser.add_argument('--taxonomy', '-T', default='metadata.yaml', help='YAML taxonomy file (default: metadata.yaml)')
+    parser.add_argument('--clean', help='Remove all input document fields not present in taxonomy (DESTRUCTIVE!)',
+                        default=False, action='store_true')
+    parser.add_argument('--noindent', help='Do not indent YAML sequences (may break other tools)',
                         default=False, action='store_true')
     parser.add_argument('--debug', help='Enable debug mode (verbose output)', action='store_true')
     args = parser.parse_args()
@@ -25,10 +26,6 @@ def main() -> int:  # returns Unix exit value
         logging.debug('Debug output enabled')
     else:
         logging.basicConfig(level=logging.INFO)
-
-    if not args.taxonomy:
-        logging.debug('No taxonomy specified, defaulting to metadata.yaml')
-        args.taxonomy = 'metadata.yaml'
 
     if not args.outpath:
         logging.debug('No output path specified, performing in-place modification')
@@ -46,18 +43,18 @@ def main() -> int:  # returns Unix exit value
     # Retrieve YAML frontmatter portion of the Obsidian doc
     metadata: dict = yaml.safe_load(obsdoc.get_frontmatter())
     logging.debug(f'Frontmatter YAML parsed as:\n{metadata}')
-    #yaml.dump(metadata, sys.stdout)  # remove after debugging
 
+    # Read YAML taxonomy file specifying metadata fields and default values
+    logging.debug(f'Attempting to read taxonomy from {args.taxonomy}')
     with open(args.taxonomy, 'r') as tf:
-        # Read YAML taxonomy file specifying metadata fields and default values
         taxo: dict = yaml.safe_load(tf)
         logging.debug(f'Taxonomy YAML parsed as:\n{taxo}')
 
     # Construct new frontmatter using taxonomy as definition
     #   - If field is in taxonomy AND in document already, keep and use document value
     #   - If field is in taxonomy BUT NOT in document already, add it and use default (taxonomy-provided) value
-    #   - IF NOT --clean: If field is in document BUT NOT in taxonomy, keep and use document value
-    #   - IF --clean: If field is in document BUT NOT in taxonomy, remove it
+    #   - IF NOT --clean: If field is in document BUT NOT in taxonomy, keep and use document value (normal operation!)
+    #   - IF --clean: If field is in document BUT NOT in taxonomy, remove it (selectable, destructive operation!)
 
     newfm: dict = {}  # new frontmatter
     keylist: list
@@ -71,29 +68,36 @@ def main() -> int:  # returns Unix exit value
         if k in metadata:
             if (type(metadata[k]) is list) or (type(taxo[k]) is list):  # if the doc *or* taxo value is a list
                 if type(metadata[k]) is str:
-                    metadata[k] = [metadata[k]]  # if either is a string, convert into 1-item list
+                    metadata[k] = [metadata[k]]  # make sure that they are *both* lists so we can combine
                 if type(taxo[k]) is str:
                     taxo[k] = [taxo[k]]
-                newfm[k] = taxo[k] + metadata[k]  # combine the lists
-                # TODO: need to deduplicate newfm - if same tag is in both doc and taxo default, shouldn't appear 2x
+                newfm[k] = list(set(taxo[k] + metadata[k]))  # combine the lists, remove duplicates with list(set([]))
             else:
                 newfm[k] = metadata[k]  # for single-valued fields, prefer value on document
         else:
-            newfm[k] = taxo[k]  # use default value from taxonomy file
+            newfm[k] = taxo[k]  # if field not on doc already, use default value from taxonomy file
 
     logging.debug(f'Keeping {len(newfm)} fields, from {len(metadata.keys())} document '
                   f'and {len(taxo.keys())} taxonomy fields '
                   f'({len([key for key in taxo.keys() or metadata.keys()])} total unique)')
     logging.debug('New metadata:\n' + str(newfm))
 
-    # Regenerate YAML frontmatter and combine with content to make new document text
+    # Serialize YAML frontmatter into lines, then replace existing frontmatter in the ObsDocument
     newfmlines: [''] = (yaml.dump(newfm, explicit_start=True, default_flow_style=False)).splitlines(keepends=True)
     obsdoc.replace_frontmatter(newfmlines)
+
+    # By default, indent YAML sequences unless --noindent is specified
+    # FIXME: this doesn't work as-is, also it probably breaks more-complex YAML than Obsidian's
+    if not args.noindent:  # args.noindent defaults to False
+        for line in newfmlines:
+            if line[:2] == '- ':
+                line = '  ' + line
+    else:
+        logging.debug('Skipping indentation due to --noindent option')
 
     # Write out frontmatter+content (YAML+Markdown) to desired output path
     with open(args.outpath, 'w') as outf:
         outf.writelines(obsdoc.lines)
-
     return 0
 
 
