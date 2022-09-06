850007fdc469cd52cbaf28608e26b401801dc9b1
Jachimo <jachimo@noreply.github.com>
2022-09-06 14:58:25 -0400

    Fix spelling in wikify_terms() comment.
    s/intenal/internal/

--- obs_document.py
+++ obs_document.py
@@ -108,7 +108,7 @@ class ObsDocument:
 
     def wikify_terms(self, termslist: ['']):
         """Make *all* occurrences of each of the list of terms into
-        an intenal wiki-type link, by enclosing in [[double brackets]]
+        an internal wiki-type link, by enclosing in [[double brackets]]
         """
         newcontent: [''] = []
         for line in self.get_content():
